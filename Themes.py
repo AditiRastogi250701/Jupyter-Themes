e07ab0d4183d756dedde20da513d431e6e8f70c8
Aditi Rastogi <72125549+AditiRastogi250701@users.noreply.github.com>
2022-09-17 02:55:04 +0530

    Python File

---
+++ Themes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install jupyterthemes
+
+
+# In[ ]:
+
+
+pip install --upgrade jupyterthemes
+
+
+# In[1]:
+
+
+get_ipython().system('jt -l')
+
+
+# In[2]:
+
+
+get_ipython().system('jt -t oceans16 -T')
+
+
+# In[ ]:
+
+
+jt -t oceans16 -T -N 
+
